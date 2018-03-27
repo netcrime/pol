6c7fdbc09350f3ebb106cc9d85224bca55294d19
Povilas <netcrime4@gmail.com>
2018-03-27 20:38:33 +0300

    Test commit

--- downloader.py
+++ downloader.py
@@ -2,6 +2,7 @@ import sys
 from pol.server import Server
 from settings import DATABASES, SNAPSHOT_DIR, DOWNLOADER_USER_AGENT, DEBUG
 
+# Povilas
 
 port = sys.argv[1] if len(sys.argv) >= 2 else 1234
 
