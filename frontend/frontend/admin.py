10d44ff6e3fcaf2b1d2cbb61ecbfdf289c3fd822
Povilas <netcrime4@gmail.com>
2018-03-27 21:28:45 +0300

    Admin panel edit

--- frontend/frontend/admin.py
+++ frontend/frontend/admin.py
@@ -5,5 +5,17 @@ from models import Feed, Field, FeedField, Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'created')
 
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ('id', 'uri', 'xpath', 'created')
+
+class FieldAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'required')
+
+class FeedFieldAdmin(admin.ModelAdmin):
+    list_display = ('id', 'feed', 'field', 'xpath')
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Feed, FeedAdmin)
+admin.site.register(Field, FieldAdmin)
+admin.site.register(FeedField, FeedFieldAdmin)
 # Register your models here.
